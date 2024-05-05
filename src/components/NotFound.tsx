25f963cf5f17b48b3664b5d683aad6c720441554
vempr <alexnguyena6@gmail.com>
2024-05-05 10:27:59 +0200

    new: hamburger menu for mobile and responsive error page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="mx-[20vw] flex flex-row gap-x-4">
-      <h1 className="text-9xl font-bold">Oops!</h1>
+    <div className="mx-10 flex flex-col justify-center gap-x-4 lg:mx-[20vw] lg:flex-row">
+      <h1 className="text-5xl font-bold lg:text-9xl">Oops!</h1>
       <p>
         It seems like the page you're looking for has wandered off into the
         digital wilderness. Let's try navigating back to safety together. How
